1a1f933d6a8613000219e603799eff7a1d2c173c
Brandy Moore <brandymoore923@gmail.com>
2022-10-13 20:45:07 -0500

    changed server file

--- server.js
+++ server.js
@@ -1,27 +1,14 @@
 const express = require('express');
-const mongodb = require('mongodb').MongoClient;
-// const mongoose = require('mongoose')
+const routes = require('./routes');
+const db = require('./config/connection');
 
+const PORT = process.env.port || 3001;
 const app = express();
-const Port = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(require('./routes'));
+app.use(routes);
 
-// MONGOOSE 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network-Api', {
-  useFindAndModify: false,
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
-
-mongoose.connection.on('connected', () =>
-console.log('Connected to MongoDB Endpoint')
-);
-
-mongoose.connection.on('error', (err) =>
-console.log(`MONGOOSE DISCONNECTED ERROR: ${err}`)
-);
-
-app.listen(PORT, () => console.log(`Now listening on localhost ${PORT}`));
+db.once('open', () => {
+  app.listen(PORT, () => console.log(`Now listening on localhost ${PORT}`));
+  });
